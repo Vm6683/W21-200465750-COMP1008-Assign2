860cf6a73297ce042090009562e3a08b9ed86d04
Vm6683 <77803227+Vm6683@users.noreply.github.com>
2021-02-11 00:08:51 -0500

    Constructor of student class. Lab - 2

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,42 @@
 public class Student {
 
-    
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+
+    /**
+     * A Consturcter with all appropriate arguments
+     */
+    public Student(String firstName, String lastName, int studentNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+    }
+
+    /**
+     * A setter and getter for each field that simply sets and gets the field value.
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public int getstudentNumber() {
+        return studentNumber;
+    }
 }
